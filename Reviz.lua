96041f226c45dff1e834cbb8e1373ac4e77854dd
JustFélix_ <85786075+LeBazarDeBryan@users.noreply.github.com>
2024-05-30 20:36:39 +0200

    Update Reviz.lua

--- Reviz.lua
+++ Reviz.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/N0ub4x/RBX_Scripts/main/Scripts/Reviz.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/LeBazarDeBryan/RBX_Scripts/main/Scripts/Reviz.lua'))()
